553652e1b39dbd5571c635ad87599d22a26241a6
wordisstuff <wordisstuff@gmail.com>
2024-04-18 00:18:30 +0000

    api

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { requestMovies } from './services/api'
 
 function App() {
   const [count, setCount] = useState(0)
+ requestMovies()
+
 
   return (
     <>
---
+++ src/services/api.js
@@ -0,0 +1,30 @@
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MjRkN2IwNzg5OTQwOGM1MTBlM2RmYzgwYTU2ZjUwOSIsInN1YiI6IjY2MjA0NzEzZWNhZWY1MDE2M2Y5Yjk5YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zePi9NJQsYyKYadY4o_bn0PQ2z4Myq9X5-gv6YSxrWI
+
+
+import axios from 'axios';
+
+const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/trending/movie/day',
+  params: {language: 'en-US'},
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MjRkN2IwNzg5OTQwOGM1MTBlM2RmYzgwYTU2ZjUwOSIsInN1YiI6IjY2MjA0NzEzZWNhZWY1MDE2M2Y5Yjk5YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zePi9NJQsYyKYadY4o_bn0PQ2z4Myq9X5-gv6YSxrWI'
+  }
+};
+
+
+export const requestMovies = async ()=>{
+    axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+} 
+
+
+
+// export const api = axios.create({baseUrl: ""})
