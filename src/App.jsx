e319814ee16a621823794482df6857feab110db9
wordisstuff <wordisstuff@gmail.com>
2024-04-18 03:54:23 +0000

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { requestMovies } from './services/api'
 import MovieList from './components/MovieList/MovieList'
-import { createLogger } from 'vite'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   const [movies, setMovies] = useState(null)
 
 
